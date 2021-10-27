31008fe71f92b4a07fe2636f19688948e56cfd36
Ahmed Riad <ahmed.riad.elashrey@gmail.com>
2021-10-27 15:02:40 +0300

    Updated validation pipe

--- src/pipes/microservice-validation.pipe.ts
+++ src/pipes/microservice-validation.pipe.ts
@@ -10,14 +10,13 @@ export class MicroserviceValidationPipe extends ValidationPipe {
     super({
       whitelist: true,
       exceptionFactory: errors =>
-        new RpcException(
-          new BadRequestException(
-            errors.map(error => ({
-              msgs: Object.values(error.constraints),
-              field: error.property,
-            })),
-          ),
+        new BadRequestException(
+          errors.map(error => ({
+            msgs: Object.values(error.constraints),
+            field: error.property,
+          })),
         ),
+
       ...options,
     });
   }
